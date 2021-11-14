d9fc34ff25e5445741e8b19b747c9d20bd90a4b5
Matthew Stanciu <mattbstanciu@gmail.com>
2021-11-13 23:16:50 -0500

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      'title': '"IBM Plex Sans", system-ui, Roboto, sans-serif',
+      'sans': '"Inter", sans-serif'
+    }
   },
   variants: {
     extend: {},
