c40dc2285a9838f72c77891c2d84a2a4ad6de69f
Matthew Stanciu <mattbstanciu@gmail.com>
2021-11-21 15:32:43 -0500

    Center text in footer
    looked weird on small screen sizes otherwise

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 import StyledLink from "./styled-link"
 
 const Footer = () => (
-  <footer className="bg-gray-100 flex flex-col justify-center items-center bottom-0 m-0 absolute w-full py-8">
+  <footer className="bg-gray-100 flex flex-col justify-center items-center text-center bottom-0 m-0 absolute w-full py-8">
     <p>Made with 💛 by the{' '}<StyledLink destination="https://purduehackers.com">Purdue Hackers</StyledLink> executive team.</p>
   </footer>
 )
