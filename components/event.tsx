cbad9826a48eeca6ee8fcf45eda38f0f65712a0a
Matthew Stanciu <mattbstanciu@gmail.com>
2021-11-21 09:39:33 -0500

    Remove bold text for time on event cards
    Not yet sure if this is a good idea

--- components/event.tsx
+++ components/event.tsx
@@ -11,7 +11,7 @@ const Event = ({ name, slug, start, end }) => (
       <div className={`col-span-1 flex flex-col rounded-lg p-5 sm:min-h-full ${past(end)
         ? "bg-gray-200" : "bg-yellow-400 hover:scale-105 transform transition"}`}>
         <p>
-          <strong>{start === 'TBD' ? 'TBD' : tt('{MM} {Do}').render(new Date(start))}</strong>{' '}
+          {start === 'TBD' ? 'TBD' : tt('{MM} {Do}').render(new Date(start))}{' '}
           {start === 'TBD' ? '' : tt('{h}:{mm}').render(new Date(start)) + "—"}
           {end === 'TBD' ? '' : tt('{h}:{mm} {a}').render(new Date(end))}
         </p>
