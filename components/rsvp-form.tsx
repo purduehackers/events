1c9356ecda02f44b44629657a7230948ab32c724
Matthew Stanciu <mattbstanciu@gmail.com>
2021-11-23 13:00:04 -0500

    Fix RSVP form button overflow on extra small screens

--- components/rsvp-form.tsx
+++ components/rsvp-form.tsx
@@ -33,10 +33,10 @@ const RSVPForm = ({ eventName, slug }) => {
   return (
     <div className="flex flex-col items-left mt-6">
       <p className="text-sm text-gray-500 dark:text-gray-400 ml-1">Enter your email</p>
-      <form onSubmit={onSubmit} className="flex flex-row items-left gap-x-2 mb-1">
+      <form onSubmit={onSubmit} className="flex flex-col xs:flex-row items-left gap-y-2 xs:gap-x-2 mb-1">
         <input type="email" name="email" id="email" value={email} placeholder="phacker@purdue.edu"
         onChange={(e) => setEmail(e.target.value)} className="rounded border-none outline-none dark:bg-gray-900 dark:text-gray-100"></input>
-        <button type="submit" className="bg-yellow-400 dark:bg-yellow-500 rounded-md shadow-md px-2 font-bold hover:scale-105 transform transition
+        <button type="submit" className="bg-yellow-400 dark:bg-yellow-500 rounded-md shadow-md py-2 xs:px-2 font-bold hover:scale-105 transform transition
         disabled:opacity-50 disabled:hover:scale-100 dark:text-black" disabled={email.length === 0}>
           {submitting ? '•••' : 'Submit'}
         </button>
