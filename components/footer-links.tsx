474ac255b95b308c1ebafae8395da87951944c09
Matthew Stanciu <mattbstanciu@gmail.com>
2021-11-23 14:21:17 -0500

    Add footer links

---
+++ components/footer-links.tsx
@@ -0,0 +1,18 @@
+import { GitHub, Home, Instagram } from 'react-feather'
+import StyledLink from "./styled-link"
+
+const FooterLinks = () => (
+  <div className="flex flex-row justify-center gap-x-5">
+    <StyledLink destination="https://purduehackers.com" newTab>
+      <Home />
+    </StyledLink>
+    <StyledLink destination="https://github.com/purduehackers" newTab>
+      <GitHub />
+    </StyledLink>
+    <StyledLink destination="https://instagram.com/purduehackers" newTab>
+      <Instagram />
+    </StyledLink>
+  </div>
+)
+
+export default FooterLinks
