767c0619554212ee2f7bd2b5223a44960641d0ce
Matthew Stanciu <mattbstanciu@gmail.com>
2021-11-23 10:21:48 -0500

    Ok I think this one will fix it

--- components/theme-button.tsx
+++ components/theme-button.tsx
@@ -4,17 +4,17 @@ import { useTheme } from 'next-themes'
 
 const ThemeButton = () => {
   const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
+  const { theme, setTheme, resolvedTheme } = useTheme()
 
   useEffect(() => setMounted(true), [])
   if (!mounted) return null
 
   return (
     <div className="p-4 mr-4 w-full xl:mr-32 flex flex-row justify-end bg-gray-100 dark:bg-gray-800">
-      {theme === 'light' && (
+      {resolvedTheme === 'light' && (
         <Moon color="black" onClick={() => setTheme('dark')} />
       )}
-      {theme === 'dark' && (
+      {resolvedTheme === 'dark' && (
         <Sun onClick={() => setTheme('light')} />
       )}
     </div>
