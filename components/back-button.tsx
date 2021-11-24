8640fca7ea96a9601d6d6065450acc3761ccb10c
Matthew Stanciu <mattbstanciu@gmail.com>
2021-11-24 14:19:18 -0500

    Make back button a bit better

--- components/back-button.tsx
+++ components/back-button.tsx
@@ -4,13 +4,13 @@ import { ArrowLeft } from 'react-feather'
 
 const BackButton = ({ bg = 'bg-gray-100 dark:bg-gray-800' }) => (
   <div className={`${bg}`}>
-    <div className="">
-    <Link href="/" passHref>
-      <a href="#" className="flex flex-row justify-left p-2 text-yellow-500 hover:text-yellow-400 transition ml-4 mr:0 xl:ml-32 py-4 w-max">
-          <ArrowLeft />
-          All Events
-      </a>
-    </Link>
+    <div className="p-4">
+      <Link href="/" passHref>
+        <a href="#" className="flex flex-row justify-left text-yellow-500 hover:text-yellow-400 transition ml-4 mr:0 xl:ml-32 w-max">
+            <ArrowLeft />
+            All Events
+        </a>
+      </Link>
     </div>
   </div>
 )
