35a04e04e905e33130143f0bad59eb57e869a9ee
Matthew Stanciu <mattbstanciu@gmail.com>
2021-12-23 19:19:33 -0500

    Simplify email link styling

--- pages/email-verification-failed.tsx
+++ pages/email-verification-failed.tsx
@@ -1,53 +1,41 @@
-import { useTheme } from 'next-themes'
 import { AlertTriangle } from 'react-feather'
 import Nav from '../components/nav'
 import StyledLink from '../components/styled-link'
 import Head from 'next/head'
 
-const EmailVerificationFailed = () => {
-  const { resolvedTheme } = useTheme()
-
-  return (
-    <main className="flex flex-col min-h-screen dark:bg-gray-900">
-      <Head>
-        <title>Email Verification Failed — Purdue Hackers</title>
-      </Head>
-      <Nav />
-      <div className="flex items-center justify-center grow py-6">
-        <div className="container mx-auto px-4 md:px-16 lg:px-72 xl:px-96">
-          <div className="rounded-lg shadow-md dark:shadow-black/25 bg-gray-200 dark:bg-gray-700 p-4 flex flex-col justify-center gap-y-4">
-            <h1 className="text-2xl sm:text-3xl font-bold text-center flex flex-wrap items-center gap-3 mb-1">
-              <AlertTriangle color="red" size="1.125em" />
-              Couldn't add you to the list :(
-            </h1>
-            <p>
-              We couldn't add you to the mailing list :( this usually happens if
-              you clicked a button on an old email.
-            </p>
-            <p>
-              Try signing up to receive reminders again on the event page. Make
-              sure to wait for a new email to arrive & click on that email's
-              button.
-            </p>
-            <p>
-              If you did that and you're still seeing this, shoot an email to{' '}
-              <StyledLink
-                destination="mailto:mstanciu@purdue.edu"
-                color={
-                  resolvedTheme === 'light'
-                    ? `text-blue-600 hover:text-blue-500`
-                    : `text-amber-500 hover:text-amber-400`
-                }
-              >
-                mstanciu@purdue.edu
-              </StyledLink>
-              .
-            </p>
-          </div>
+const EmailVerificationFailed = () => (
+  <main className="flex flex-col min-h-screen dark:bg-gray-900">
+    <Head>
+      <title>Email Verification Failed — Purdue Hackers</title>
+    </Head>
+    <Nav />
+    <div className="flex items-center justify-center grow py-6">
+      <div className="container mx-auto px-4 md:px-16 lg:px-72 xl:px-96">
+        <div className="rounded-lg shadow-md dark:shadow-black/25 bg-gray-200 dark:bg-gray-700 p-4 flex flex-col justify-center gap-y-4">
+          <h1 className="text-2xl sm:text-3xl font-bold text-center flex flex-wrap items-center gap-3 mb-1">
+            <AlertTriangle color="red" size="1.125em" />
+            Couldn't add you to the list :(
+          </h1>
+          <p>
+            We couldn't add you to the mailing list :( this usually happens if
+            you clicked a button on an old email.
+          </p>
+          <p>
+            Try signing up to receive reminders again on the event page. Make
+            sure to wait for a new email to arrive & click on that email's
+            button.
+          </p>
+          <p>
+            If you did that and you're still seeing this, shoot an email to{' '}
+            <StyledLink destination="mailto:mstanciu@purdue.edu">
+              mstanciu@purdue.edu
+            </StyledLink>
+            .
+          </p>
         </div>
       </div>
-    </main>
-  )
-}
+    </div>
+  </main>
+)
 
 export default EmailVerificationFailed
