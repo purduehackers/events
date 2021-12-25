19378cd50924c087f2880f7273c6a57d82bc0da4
Matthew Stanciu <mattbstanciu@gmail.com>
2021-12-25 18:38:57 -0500

    Clean up footer component code

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,7 @@
-const Footer = ({ ...props }) => {
-  return (
-    <div className="bg-gray-100 dark:bg-gray-800 text-center dark:text-gray-100 bottom-0 mt-auto w-full flex flex-col justify-center gap-y-4 py-8 px-4">
-      {props.children}
-    </div>
-  )
-}
+const Footer = ({ ...props }) => (
+  <div className="bg-gray-100 dark:bg-gray-800 text-center dark:text-gray-100 bottom-0 mt-auto w-full flex flex-col justify-center gap-y-4 py-8 px-4">
+    {props.children}
+  </div>
+)
 
 export default Footer
