c56dbfddc72c09873678c962b710374939e0f192
Matthew Stanciu <mattbstanciu@gmail.com>
2021-12-25 14:29:03 -0500

    Fix footer! & some styling changes

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = ({ ...props }) => {
   return (
-    <div className="bg-gray-100 dark:bg-gray-800 text-center dark:text-gray-100 bottom-0 m-0 absolute w-full flex flex-col justify-center gap-y-4 py-8 px-4">
+    <div className="bg-gray-100 dark:bg-gray-800 text-center dark:text-gray-100 bottom-0 mt-auto w-full flex flex-col justify-center gap-y-4 py-8 px-4">
       {props.children}
     </div>
   )
