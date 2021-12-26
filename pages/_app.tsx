2d92181aee94259c388ce94a46ee74e611671c7a
Matthew Stanciu <mattbstanciu@gmail.com>
2021-12-26 18:06:23 -0500

    Add dynamic OG images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,12 @@ function Events({ Component, pageProps }: AppProps) {
     <ThemeProvider attribute="class">
       <Head>
         <meta name="theme-color" content="#D97706" />
+        <meta property="og:title" content="Events — Purdue Hackers" />
+        <meta property="og:url" content="https://events.purduehackers.com" />
+        <meta
+          property="og:description"
+          content="Check out & sign up for upcoming events from Purdue Hackers."
+        />
       </Head>
       <Component {...pageProps} />
     </ThemeProvider>
