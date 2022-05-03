a8ed1109d0f2a557d69032b1aa787cb77f1c0996
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-03 13:54:15 -0400

    ▲ Powered by Vercel

---
+++ components/vercel-logo.tsx
@@ -0,0 +1,12 @@
+const VercelLogo = () => (
+  <div className="mx-auto">
+    <a
+      href="https://vercel.com?utm_source=purduehackers&utm_campaign=oss"
+      target="_blank"
+    >
+      <img src="powered-by-vercel.svg" />
+    </a>
+  </div>
+)
+
+export default VercelLogo
