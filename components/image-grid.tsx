9f218974e4df468fc719dc83d1935c0ecbe52d8a
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-11 15:53:30 -1000

    Fix small styling thing

--- components/image-grid.tsx
+++ components/image-grid.tsx
@@ -5,7 +5,7 @@ const ImageGrid = ({ images = [] }: { images: Array<AirtableAttachment> }) => {
   images = images.filter((image) => image.width > image.height)
   return (
     <div className="flex flex-col gap-y-4 items-center justify-center">
-      <div className="grid grid-cols-1 gap-y-2 md:grid-cols-3 md:gap-x-2 w-3/4">
+      <div className="grid grid-cols-1 gap-y-2 md:grid-cols-3 md:gap-x-2 md:w-3/4">
         {images.map((image, i) => {
           return i < 3 && <ImageCard image={image} />
         })}
