50c359f8fb0c7273203b00c1e169f3d52ddd0780
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-12 11:11:58 -1000

    Remove location data & make h1 amber again

--- components/past-event.tsx
+++ components/past-event.tsx
@@ -47,7 +47,7 @@ const PastEvent = ({ event }: { event: PHEvent }) => {
 
       <div className="flex flex-col grow-0 items-center justify-top mt-0 w-full flex-1 px-5 pb-8 sm:pb-16 text-center sm:px-20 bg-gray-100 dark:bg-gray-800">
         <div className="mt-8 sm:mt-16">
-          <h1 className="text-4xl sm:text-7xl lg:text-8-xl font-bold text-gray-400">
+          <h1 className="text-4xl sm:text-7xl lg:text-8-xl font-bold text-amber-400 dark:text-amber-500">
             {event.name}
           </h1>
           <p className="mt-3 text-1xl sm:text-2xl flex flex-row gap-x-1 font-bold items-center justify-center dark:text-gray-200">
@@ -61,20 +61,6 @@ const PastEvent = ({ event }: { event: PHEvent }) => {
             {event.end === 'TBD'
               ? ''
               : tt('{h}:{mm} {a}').render(new Date(event.end))}
-          </p>
-          <p className="mt-1 text-1xl sm:text-2xl flex flex-row gap-x-1 items-center justify-center dark:text-gray-200">
-            <MapPin />
-            <strong>
-              {event.loc === 'TBD' ? (
-                'Location TBD'
-              ) : event.gMap ? (
-                <StyledLink destination={event.gMap} newTab>
-                  {event.loc}
-                </StyledLink>
-              ) : (
-                event.loc
-              )}
-            </strong>
           </p>
         </div>
       </div>
