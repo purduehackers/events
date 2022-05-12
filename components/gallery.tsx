a56ecc7f055e9c8469cb7dd6946f750e27398b40
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-11 17:29:14 -1000

    Remove react-modal for now
    Was doing something funk with types I think(?) and causing the build to fail

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -1,21 +1,22 @@
-import Modal from 'react-modal'
-import { XCircle } from 'react-feather'
+// import Modal from 'react-modal'
+// import { XCircle } from 'react-feather'
 
-Modal.setAppElement('#__next')
+// Modal.setAppElement('#__next')
 
-const Gallery = ({
-  open,
-  images,
-  onClose,
-  ...props
-}: {
-  open: boolean
-  images: Array<AirtableAttachment>
-  onClose: Function
-}) => (
-  <Modal key="modal" isOpen={open}>
-    <div className="bg-gray-900"></div>
-  </Modal>
-)
+// const Gallery = ({
+//   open,
+//   images,
+//   onClose,
+//   ...props
+// }: {
+//   open: boolean
+//   images: Array<AirtableAttachment>
+//   onClose: Function
+// }) => (
+//   <Modal key="modal" isOpen={open}>
+//     <div className="bg-gray-900"></div>
+//   </Modal>
+// )
 
-export default Gallery
+// export default Gallery
+export {}
