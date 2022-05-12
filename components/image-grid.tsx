a9fd0f09c6423823ba376fb156d8c4967b9571de
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-11 16:33:41 -1000

    Fix layout on small screens

--- components/image-grid.tsx
+++ components/image-grid.tsx
@@ -4,7 +4,7 @@ import StyledButton from './styled-button'
 const ImageGrid = ({ images = [] }: { images: Array<AirtableAttachment> }) => {
   images = images.filter((image) => image.width > image.height)
   return (
-    <div className="flex flex-col gap-y-4 items-center">
+    <div className="flex flex-col gap-y-4 px-4 items-center">
       <div className="grid grid-cols-1 gap-y-2 md:grid-cols-3 md:gap-x-2 md:w-3/4 items-center">
         {images.map((image, i) => {
           return i < 3 && <ImageCard image={image} />
