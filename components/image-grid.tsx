db91f0576fc04f24cf898200758b9011ec837125
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-11 15:48:51 -1000

    Fix grid

--- components/image-grid.tsx
+++ components/image-grid.tsx
@@ -4,8 +4,8 @@ import StyledButton from './styled-button'
 const ImageGrid = ({ images = [] }: { images: Array<AirtableAttachment> }) => {
   images = images.filter((image) => image.width > image.height)
   return (
-    <div className="flex flex-col gap-y-4">
-      <div className="flex flex-col flex-wrap space-y-2 md:flex-row md:space-x-2">
+    <div className="flex flex-col gap-y-4 items-center justify-center">
+      <div className="grid grid-cols-1 gap-y-2 md:grid-cols-3 md:gap-x-2 w-3/4">
         {images.map((image, i) => {
           return i < 3 && <ImageCard image={image} />
         })}
