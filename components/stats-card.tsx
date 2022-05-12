dc59206599e748905b62fbca1dd9cc3f13b7734c
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-12 11:09:32 -1000

    Make card slightly bigger on small screens

--- components/stats-card.tsx
+++ components/stats-card.tsx
@@ -1,6 +1,6 @@
 const StatsCard = ({ event }: { event: PHEvent }) => (
   <div
-    className={`flex flex-col md:flex-row items-center gap-8 border-double border-8 border-amber-400 dark:border-amber-500 rounded mx-20 md:mx-auto mb-8 p-8 text-center ${
+    className={`flex flex-col md:flex-row items-center gap-8 border-double border-8 border-amber-400 dark:border-amber-500 rounded mx-16 md:mx-auto mb-8 p-8 text-center ${
       event.stat1Data || event.stat2Data || event.stat3Data ? '' : 'hidden'
     }`}
   >
