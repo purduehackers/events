11e9fc1735128cebfc4ec7fd91db65ec67f95a39
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-12 17:06:40 -1000

    Resize large images

--- components/image-card.tsx
+++ components/image-card.tsx
@@ -1,17 +1,30 @@
 import Image from 'next/image'
 
-const ImageCard = ({ image }: { image: AirtableAttachment }) => (
-  <div className="flex flex-col mx-auto">
-    <Image
-      src={image.url}
-      width={image.width}
-      height={image.height}
-      key={image.url}
-      priority
-      className="rounded"
-    />
-    {/* <div className="bg-gray-700 text-center py-3 rounded-b">Hi</div> */}
-  </div>
-)
+const resizeImage = (image: AirtableAttachment): AirtableAttachment => {
+  if (image.width > 2100) {
+    image.width /= 1.5
+    image.height /= 1.5
+    return resizeImage(image)
+  }
+
+  return image
+}
+
+const ImageCard = ({ image }: { image: AirtableAttachment }) => {
+  image = resizeImage(image)
+  return (
+    <div className="flex flex-col mx-auto">
+      <Image
+        src={image.url}
+        width={image.width}
+        height={image.height}
+        key={image.url}
+        priority
+        className="rounded"
+      />
+      {/* <div className="bg-gray-700 text-center py-3 rounded-b">Hi</div> */}
+    </div>
+  )
+}
 
 export default ImageCard
