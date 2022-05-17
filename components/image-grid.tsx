fd0ea825e80fc3e9a1400ffe87715088642db9e1
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-17 11:00:17 +0300

    Make image grid better on small screens
    Fixes #17

--- components/image-grid.tsx
+++ components/image-grid.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import useMediaQuery from '../lib/hooks/use-media-query'
 import Gallery from './gallery'
 import ImageCard from './image-card'
 
@@ -31,19 +32,16 @@ const ImageGrid = ({ images = [] }: { images: Array<AirtableAttachment> }) => {
     }
   })
 
-  const [smallScreen, setSmallScreen] = useState(false)
+  const smallScreen = useMediaQuery('(max-width:768px)')
+  const [open, setOpen] = useState(false)
+  const [index, setIndex] = useState(0)
   const [shuffledImages, setShuffledImages] = useState<Array<GridImage>>([
     { image: filteredImages[0].image, index: filteredImages[0].index }
   ])
+
   useEffect(() => {
     setShuffledImages(shuffle(filteredImages))
-    if (window.innerWidth < 640) {
-      setSmallScreen(true)
-    }
   }, [])
-
-  const [open, setOpen] = useState(false)
-  const [index, setIndex] = useState(0)
 
   const onClose = () => {
     setOpen(false)
@@ -56,39 +54,45 @@ const ImageGrid = ({ images = [] }: { images: Array<AirtableAttachment> }) => {
 
   return (
     <div className="flex flex-col gap-y-4 mx-4 sm:mx-0 items-center">
-      <div className="grid grid-cols-1 gap-y-2 md:grid-cols-3 md:gap-x-2 sm:max-w-lg md:max-w-xl items-center">
-        {smallScreen ? (
-          <ImageCard
-            image={shuffledImages[0].image}
-            index={shuffledImages[0].index}
-            click={click}
-          />
-        ) : (
-          shuffledImages.map(
-            (image, i) =>
-              i < 3 && (
-                <ImageCard
-                  image={image.image}
-                  index={image.index}
-                  click={click}
-                  key={image.image.url}
-                />
-              )
-          )
+      <div className="grid grid-cols-2 gap-2 md:grid-cols-3 md:gap-x-2 sm:max-w-lg md:max-w-xl items-center">
+        {shuffledImages.map(
+          (image, i) =>
+            i < 3 && (
+              <ImageCard
+                image={image.image}
+                index={image.index}
+                click={click}
+                key={image.image.url}
+              />
+            )
         )}
+        <button
+          className="sm:hidden rounded-lg mx-auto py-2 px-2 font-bold text-l dark:text-gray-200 shadow-md dark:shadow-black/25 border-solid border-2 border-amber-400 dark:border-amber-500 p-2 px-4 text-center hover:scale-105 transform transition"
+          onClick={(e) => {
+            if (!e.metaKey) {
+              e.preventDefault()
+              setIndex(0)
+              setOpen(true)
+            }
+          }}
+        >
+          See all photos
+        </button>
       </div>
-      <button
-        className="rounded-lg mx-auto py-2 px-2 font-bold text-l dark:text-gray-200 shadow-md dark:shadow-black/25 border-solid border-2 border-amber-400 dark:border-amber-500 p-2 px-4 text-center hover:scale-105 transform transition"
-        onClick={(e) => {
-          if (!e.metaKey) {
-            e.preventDefault()
-            setIndex(0)
-            setOpen(true)
-          }
-        }}
-      >
-        See all photos
-      </button>
+      {!smallScreen && (
+        <button
+          className="rounded-lg mx-auto py-2 px-2 font-bold text-l dark:text-gray-200 shadow-md dark:shadow-black/25 border-solid border-2 border-amber-400 dark:border-amber-500 p-2 px-4 text-center hover:scale-105 transform transition"
+          onClick={(e) => {
+            if (!e.metaKey) {
+              e.preventDefault()
+              setIndex(0)
+              setOpen(true)
+            }
+          }}
+        >
+          See all photos
+        </button>
+      )}
       <Gallery
         images={images}
         index={index}
