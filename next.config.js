5eea4c64c5d5c85355d0c9efc27b465b086ee876
Matthew Stanciu <mattbstanciu@gmail.com>
2022-05-17 11:37:59 +0300

    Redirect archives/* –> archive/*

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   images: {
     domains: ['dl.airtable.com']
+  },
+  async redirects() {
+    return [
+      {
+        source: '/archives/:slug*',
+        destination: '/archive/:slug*',
+        permanent: true
+      }
+    ]
   }
 }
