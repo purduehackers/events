c2e00803582683778e56f60cf9c1d6a7688c594b
Matthew Stanciu <mattbstanciu@gmail.com>
2022-08-20 13:54:19 -0400

    Make upcoming event card a bit wider

--- components/desc-box.tsx
+++ components/desc-box.tsx
@@ -1,5 +1,5 @@
 const DescriptionBox = ({ ...props }) => (
-  <div className="container mx-auto p-8 px-4 md:px-16 lg:px-72 xl:px-96">
+  <div className="container mx-auto p-8 px-4 md:px-16 lg:px-64 xl:px-96">
     <div className="border-2 border-dashed rounded-lg p-4 border-amber-400 dark:border-amber-500">
       {props.children}
     </div>
