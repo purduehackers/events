ca8956d58cbcd79437889d3eadb7e65cd9b902c2
Matthew Stanciu <mattbstanciu@gmail.com>
2022-09-16 19:47:15 -0400

    Fix card

--- components/event-card.tsx
+++ components/event-card.tsx
@@ -29,8 +29,12 @@ const EventCard = ({
               .replace('day', '')
               .replace('nes', '')
               .replace('Satur', 'Sat')}{' '}
-        {start === 'TBD' ? '' : tt('{h}:{mm}').render(new Date(start)) + '—'}
-        {end === 'TBD' ? '' : tt('{h}:{mm} {a}').render(new Date(end))}
+        {start === 'TBD'
+          ? ''
+          : tt(`{h}:{mm}${end === 'TBD' ? ' {a}' : ''}`).render(
+              new Date(start)
+            ) + '—'}
+        {end === 'TBD' ? '???' : tt('{h}:{mm} {a}').render(new Date(end))}
       </p>
       <h3
         className={`${name.length < 30 ? 'text-2xl' : 'text-2xl sm:text-xl'} ${
