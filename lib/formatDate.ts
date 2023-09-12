f8f9831b85a1dca50393fa6789497b442f48a209
Matthew Stanciu <mattbstanciu@gmail.com>
2023-09-11 22:06:50 -0400

    Show AM/PM on event card start string
    ...if event spans across multiple days

--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -1,2 +1,19 @@
 export const formatDateTz = (date: Date, tzString: string) =>
   new Date(date.toLocaleString('en-US', { timeZone: tzString }))
+
+export const eventSpansAcrossMultipleDays = (
+  start: string,
+  end: string
+): boolean => {
+  const startDate = new Date(start).getDate()
+  const endDate = new Date(end).getDate()
+  return startDate !== endDate
+}
+
+export const startTimeFormatString = (start: string, end: string): string => {
+  let baseString = 'h:mm'
+  if (end === 'TBD' || eventSpansAcrossMultipleDays(start, end)) {
+    baseString += ' a'
+  }
+  return baseString
+}
