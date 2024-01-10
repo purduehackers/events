984111136182d5876016c56957082b42a060c03a
Matthew Stanciu <mattbstanciu@gmail.com>
2024-01-09 22:24:47 -0500

    Try some workaround

--- next.config.js
+++ next.config.js
@@ -1,14 +1,19 @@
 module.exports = {
   images: {
-    domains: ['cdn.sanity.io']
+    domains: ['cdn.sanity.io'],
+  },
+  modularizeImports: {
+    '@icons-pack/react-simple-icons': {
+      transform: '@icons-pack/react-simple-icons/icons/{{member}}',
+    },
   },
   async redirects() {
     return [
       {
         source: '/archives/:slug*',
         destination: '/archive/:slug*',
-        permanent: true
-      }
+        permanent: true,
+      },
     ]
-  }
+  },
 }
