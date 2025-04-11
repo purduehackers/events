a2cc79e9dce92b0ebb7765b694559b9cdfff3ca1
Eric Park <me@ericswpark.com>
2025-04-11 17:18:41 -0400

    api: sendReminder: use type imports

--- pages/api/sendReminder.ts
+++ pages/api/sendReminder.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 import { createClient } from 'next-sanity'
 import { Resend } from 'resend'
 
