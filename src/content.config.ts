fba9a80166417e9cb945825deeba5ca44ddc30d5
Eric Park <me@ericswpark.com>
2025-08-09 19:22:21 -0400

    Modify events schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,10 +5,11 @@ const events = defineCollection({
   loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/events" }),
   schema: z.object({
     title: z.string(),
-    datetime: z.coerce.date(),
-    end_datetime: z.coerce.date().optional(),
+    start: z.coerce.date(),
+    end: z.coerce.date().optional(),
     location: z.string().optional(),
     location_url: z.string().optional(),
+    images: z.array(z.string()).optional(),
   }),
 });
 
