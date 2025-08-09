e386a84a49d3f1f22aed72adba2642182413b200
Eric Park <me@ericswpark.com>
2025-08-09 19:08:09 -0400

    Miscellaneous fixes to conversion script

--- src/content/convert.py
+++ src/content/convert.py
@@ -35,7 +35,12 @@ def get_slug(event):
                 version = match.group(1)
         case "workshops":
             # Remove "Workshop" or "Workshops" from event name, convert spaces to dashes and lowercase
-            version = event_name.lower().replace("workshops", "").replace("workshop", "").strip()
+            version = (
+                event_name.lower()
+                .replace("workshops", "")
+                .replace("workshop", "")
+                .strip()
+            )
             version = "-".join(version.split())
         case "callouts":
             # Use term (example: Spring 2025), convert space to dash and lowercase
@@ -44,9 +49,6 @@ def get_slug(event):
 
     return class_name, version
 
-
-def is_jpg_format(format):
-    return format.upper() == "JPEG"
 
 def is_jpg_extension(extension):
     return extension in ("jpg", "jpeg")
@@ -57,25 +59,16 @@ def get_extension_from_pil(path):
     Use PIL (Pillow) to check the actual image format
     This relies on the magic number and other heuristics from the file contents
     """
-    full_file_name = os.path.basename(path)
-    _, extension = os.path.splitext(full_file_name)
-
-    try:
-        with Image.open(path) as img:
-            fmt = img.format
-            if is_jpg_format(fmt):
-                match = is_jpg_extension(extension)
-            else:
-                match = fmt.lower() == extension
-            if not match:
-                print(f"Mismatch: {full_file_name} (extension: {extension}, format: {fmt})")
-                mismatch_count += 1
-            total_images += 1
-    except Exception as e:
-        print(f"Could not open {full_file_name}: {e}")
+    with Image.open(path) as img:
+        return img.format.lower()
 
 
 def is_invalid_image_extension(path, ext):
+    pil_ext = get_extension_from_pil(path)
+
+    if is_jpg_extension(pil_ext) and is_jpg_extension(ext):
+        return True
+
     return get_extension_from_pil(path) == ext
 
 
@@ -98,30 +91,36 @@ def process_and_copy_images(event, target_path):
             print(f"WARNING: image ref is empty for image key {image_key}!")
             continue
 
-        _, image_id, _, image_ext = image_ref.split('-')
+        _, image_id, _, image_ext = image_ref.split("-")
 
         image_file_name = f"{image_id}.{image_ext}"
         base_image_full_path = os.path.join(base_image_path, image_file_name)
 
         if not os.path.exists(base_image_full_path):
-            print(f"ERROR: file does not exist at {base_image_full_path} for image key {image_key}!")
+            print(
+                f"ERROR: file does not exist at {base_image_full_path} for image key {image_key}!"
+            )
             continue
 
         # Check if Sanity has modified the file extension w/o format conversion
         correct_extension = None
         if is_invalid_image_extension(base_image_full_path, image_ext):
             correct_extension = get_extension_from_pil(base_image_full_path)
-            print(f"Warning: Sanity mangled the extension from {correct_extension} to {image_ext} for image key {image_key}.")
+            print(
+                f"Warning: Sanity mangled the extension from {correct_extension} to {image_ext} for image key {image_key}."
+            )
             print(f"\tCorrecting to {image_id}.{correct_extension}...")
         target_extension = correct_extension if correct_extension else image_ext
-        
+
         original_filename = get_image_metadata_mapping()[image_file_name]
         original_filename_base, _ = os.path.splitext(original_filename)
         new_filename = f"{original_filename_base}.{target_extension}"
 
         target_image_full_path = os.path.join(target_image_path, new_filename)
         if os.path.exists(target_image_full_path):
-            raise Exception(f"File with same file name found! Check {target_image_full_path}")
+            raise Exception(
+                f"File with same file name found! Check {target_image_full_path}"
+            )
         shutil.copy2(base_image_full_path, target_image_full_path)
 
         # Add image to be included into Markdown metadata
@@ -134,16 +133,16 @@ def get_image_metadata_mapping():
     path = os.path.join(DOWNLOAD_DIR, "images_metadata.json")
     with open(path, "r", encoding="UTF-8") as metadata_file:
         metadata = json.load(metadata_file)
-    
+
     results = metadata.get("result", [])
 
-    mappings = []
-    
+    mappings = {}
+
     for result in results:
-        file_name = f"{result.assetId}.{result.extension}"
-        original_filename = result.originalFilename
-        mappings.append({file_name: original_filename})
-    
+        file_name = f"{result["assetId"]}.{result["extension"]}"
+        original_filename = result["originalFilename"]
+        mappings[file_name] = original_filename
+
     return mappings
 
 
@@ -156,7 +155,7 @@ def save_markdown_metadata(event, images, target_path):
     preferred_event_description = past_description or og_description or desc
 
     start = event.get("start")
-    end = event.get("end") # may be None, then it won't be included in metadata
+    end = event.get("end")  # may be None, then it won't be included in metadata
     name = event.get("name")
     location_name = event.get("loc")
     location_url = event.get("gMap")
@@ -186,14 +185,16 @@ def save_markdown_metadata(event, images, target_path):
 
 def process_event(event):
     event_class, event_version = get_slug(event)
-    target_slug_path = f"/{event_class}/{event_version}/"
+    target_slug_path = f"./events/{event_class}/{event_version}/"
     images = process_and_copy_images(event, target_slug_path)
     save_markdown_metadata(event, images, target_slug_path)
 
 
 def main():
     # Read in all events
-    with open(os.path.join(DOWNLOAD_DIR, "response_filtered.json"), "r", encoding="utf-8") as file:
+    with open(
+        os.path.join(DOWNLOAD_DIR, "response_filtered.json"), "r", encoding="utf-8"
+    ) as file:
         data = json.load(file)
 
     events = data.get("result", [])
@@ -202,5 +203,5 @@ def main():
         process_event(event)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
