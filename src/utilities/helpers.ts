759bf8391e7c290fbb4e9ec3bdd552b23848a64a
Eric Park <me@ericswpark.com>
2025-08-17 00:25:04 -0400

    feat: initial implementation migration of event detail page

--- src/utilities/helpers.ts
+++ src/utilities/helpers.ts
@@ -1,5 +1,9 @@
+import { format } from "date-fns";
 
 export function getEventSlug(path: string) {
     return path.split('/').slice(2).slice(0, -1).join('/');
 }
 
+export function getTime(time: string | undefined) {
+    return time ? format(time, "hh:mm a") : "???";
+}
