11e681db66672a6c0dfa8a6c23f7eb0489691629
Eric Park <me@ericswpark.com>
2025-08-20 12:54:57 -0400

    fix: localize all times to Indiana time

--- src/utilities/helpers.ts
+++ src/utilities/helpers.ts
@@ -1,9 +1,14 @@
+import { TZDate } from "@date-fns/tz";
 import { format } from "date-fns";
 
 export function getEventSlug(path: string) {
     return path.split('/').slice(2).slice(0, -1).join('/');
 }
 
-export function getTime(time: string | undefined) {
+export function getTime(time: TZDate | string | undefined) {
     return time ? format(time, "hh:mm a") : "???";
+}
+
+export function getLocalizedDate(date: string): TZDate {
+    return new TZDate(date, "America/Indiana/Indianapolis");
 }
