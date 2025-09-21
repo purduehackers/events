b2fdaca2668ac94019641cbdd4465053e098dbc7
Eric Park <me@ericswpark.com>
2025-09-21 18:52:55 -0400

    feat: add social media & OpenGraph tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://events.purduehackers.com",
   vite: { plugins: [tailwindcss()] },
 });
