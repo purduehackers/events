b0378751a967847af5118a9bd2de98219f254323
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2026-03-01 16:31:04 -0500

    pull events from payload, generate pages from id

---
+++ src/types.ts
@@ -0,0 +1,37 @@
+interface ImageType {
+    url: string;
+    alt?: string;
+}
+
+interface StatType {
+    data: string;
+    label: string;
+}
+
+export interface EventType {
+    id: string;
+    name: string;
+    eventType: string;
+    start: string;
+    end: string;
+    location_name: string;
+    location_url: string;
+    stats: StatType[];
+    description: string;
+    images: ImageType[];
+    updatedAt: string;
+    createdAt: string;
+}
+
+export interface PayloadResponse<T> {
+    docs: T[];
+    totalDocs: number;
+    limit: number;
+    totalPages: number;
+    page: number;
+    pagingCounter: number;
+    hasPrevPage: boolean;
+    hasNextPage: boolean;
+    prevPage: number | null;
+    nextPage: number | null;
+}
