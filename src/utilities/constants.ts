dfdc0681443e635645f53ed0b1b039eb8c80aafb
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2026-03-07 17:20:24 -0500

    added cms url to constants

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,6 +1,6 @@
 
 
 // Base API url for the payload CMS instance
-export const ROOT_URL = import.meta.env.PUBLIC_ROOT_URL ?? "https://cms.purduehackers.com";
+export const CMS_URL = import.meta.env.PUBLIC_CMS_URL ?? "https://cms.purduehackers.com";
 
 export const DISCORD_URL = "https://puhack.horse/discord";
