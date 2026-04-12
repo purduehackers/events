43b3f291dc1eaab0ffdb9aa78445c5e6d8a5d869
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2026-04-12 01:17:59 -0400

    show hack night cta even when no other upcoming events found

--- src/components/SemesterEvents.tsx
+++ src/components/SemesterEvents.tsx
@@ -109,9 +109,9 @@ export default function SemesterEvents({ initialEvents, semester, currentSemeste
 
             {/* Event cards */}
             <div className="pl-(--line-card-gap) border-l-1 border-gray-300">
-                {(filteredEvents.length > 0) ?
+                {(filteredEvents.length > 0 || (currentSemester && !searchQuery && (!selectedCategory || selectedCategory === "hack-night"))) ?
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 sm:auto-cols-fr">
-                        {(currentSemester && (!selectedCategory || selectedCategory === "hack-night") && (!searchQuery || "hack night".includes(searchQuery.toLowerCase()))) &&
+                        {(currentSemester && !searchQuery && (!selectedCategory || selectedCategory === "hack-night")) &&
                             <div className="w-full flex justify-start items-center">
                                 <a className="w-full" href="https://discord.com/invite/5paFjKzdPE" target="_blank" rel="noreferrer">
                                     <div className="w-full md:w-fit rounded-sm bg-black dark:bg-yellow text-white dark:text-black p-4 flex flex-col justify-center gap-y-3">
