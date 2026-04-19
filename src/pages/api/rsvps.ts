5063f3269515fbec08d6851488c75b160f391326
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2026-04-19 18:00:59 -0400

    add rsvps route, filter for only publisehd events

--- src/pages/api/rsvps.ts
+++ src/pages/api/rsvps.ts
@@ -3,19 +3,19 @@ export const prerender = false;
 import type { APIRoute } from 'astro';
 import { CMS_URL } from "@/utilities/constants";
 
-// Get events
-export const GET: APIRoute = async ({ url }) => {
+// Create new rsvp
+export const POST: APIRoute = async ({ request }) => {
     try {
-        // Filter for published only
-        url.searchParams.set("where[published][equals]", "true");
-
-        // Make authorized fetch request
-        const baseUrl = `${CMS_URL}/api/events${url.search}`;
+        const baseUrl = `${CMS_URL}/api/rsvps`;
         const apiKey = import.meta.env.PAYLOAD_API_KEY;
+        const body = await request.json();
+
         const cmsRes = await fetch(baseUrl, {
             headers: {
                 Authorization: `service-accounts API-Key ${apiKey}`,
             },
+            method: "POST",
+            body
         });
 
         const text = await cmsRes.text();
@@ -28,7 +28,7 @@ export const GET: APIRoute = async ({ url }) => {
         });
     } catch (err) {
         return new Response(
-            JSON.stringify({ error: 'Failed to fetch events' }),
+            JSON.stringify({ error: 'Failed to create rsvp' }),
             { status: 500 }
         );
     }
