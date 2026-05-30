f780933586b0b4385f18c5652c7178684bcf002d
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2026-05-30 00:15:41 -0700

    ok last one

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  //output: "server",
+  output: "server",
   site: "https://events.purduehackers.com",
   adapter: vercel({
     isr: {
