81206566b3439a5919fc968e3067f7c672b17732
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2026-05-30 01:06:32 -0700

    bru

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
   adapter: vercel({
     isr: {
       expiration: 60 * 5,
-      bypassToken: "123",
+      bypassToken: "123123123123123123123123123123123123",
       exclude: ['/api/events', '/api/rsvps'] 
     },
   }),
