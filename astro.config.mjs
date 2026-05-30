6d11ba9ab863bf770d1870339c17e0f765390195
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2026-05-29 20:43:19 -0700

    fix event slug pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,12 @@ import vercel from "@astrojs/vercel";
 export default defineConfig({
   output: "server",
   site: "https://events.purduehackers.com",
-   adapter: vercel(),
+  adapter: vercel(/*{
+    isr: {
+      expiration: 60 * 5,
+      exclude: ['/api/events', '/api/rsvps'] 
+    },
+  }*/),
   vite: {
     plugins: [tailwindcss()],
     /*server: {
