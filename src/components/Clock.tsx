12e6bbe177312b52b371bb2cdc24270dbe1118f4
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2026-06-01 23:59:09 -0700

    update clock, better homelayout formatting

---
+++ src/components/Clock.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+const formatter = new Intl.DateTimeFormat('en-US', {
+    timeZone: "America/Indianapolis",
+    weekday: 'long',
+    month: 'short',
+    day: 'numeric',
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZoneName: "short"
+});
+
+export default function Clock() {
+    const [time, setTime] = useState(formatter.format(new Date()));
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(formatter.format(new Date()));
+        }, 1000);
+
+        return () => clearInterval(timer); 
+    }, []);
+
+    return <span>{time}</span>;
+}
